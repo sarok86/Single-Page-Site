cd0f03d95024c131164a0fdc6e7394b58a7857f7
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-08 11:44:46 +0330

    First Commit

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link} from 'react-router-dom';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li><Link to="/">HomePage</Link></li>
+                    <li><Link to="/blogs">Blogs</Link></li>
+                    <li><Link to="/products">Products</Link></li>
+                    <li><Link to="/aboutus">About Us</Link></li>
+                </ul> 
+            </div>
+        );
+    }
+}
+
+export default Navbar;
---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import  {createRoot}  from "react-dom/client"
+import App from "./App";
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
+    </StrictMode>
+)
