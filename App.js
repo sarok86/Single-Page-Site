c440e0449d8dbd550528c29355d9a5774a47b874
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-09 10:42:44 +0330

    add Redirect to file app and new file NotFound

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect } from 'react-router-dom';
 
 import HomePage from './components/HomePage';
 import Products from './components/Products';
@@ -7,6 +7,7 @@ import Blogs from './components/Blogs';
 import AboutUs from './components/AboutUs';
 import Navbar from './components/Navbar';
 import Product from './components/Product';
+import NotFound from './components/NotFound';
 
 class App extends Component {
     render() {
@@ -19,7 +20,10 @@ class App extends Component {
                         <Route path="/blogs/:author?" render={(props) => <Blogs name="Sarok" {...props}/>}/>
                         <Route path="/products" component={Products} />
                         <Route path="/aboutus" component={AboutUs} />
-                        <Route path="/" component={HomePage} />
+                        <Route path="/notfound" component={NotFound} />
+                        <Redirect from='/articles' to="/blogs" />
+                        <Route exact path="/" component={HomePage} />
+                        <Redirect to="/notfound"/>
                     </Switch>
                     {/* exact */}
                 </div>
