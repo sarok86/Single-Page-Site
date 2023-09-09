e1d3f57e9efd723ea775a599eaf69ca89bef09f4
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-09 11:10:59 +0330

    add props.history to file AboutUs

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class AboutUs extends Component {
+    reset = () => {
+        this.props.history.replace("/blogs")
+        // this.props.history.push("/blogs")
+    }
     render() {
         return (
             <div>
                 <h1>AboutUs</h1>
+                <button onClick={this.reset}>Reset</button>
             </div>
         );
     }
