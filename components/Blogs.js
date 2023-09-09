b9e70a5c35457c979ed61f157e5ae4259e864021
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-09 10:01:07 +0330

    add query string to blogs

--- components/Blogs.js
+++ components/Blogs.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import queryString from "query-string"
 
 class Blogs extends Component {
+
+    parseQuery = () =>{
+        console.log(this.props.location.search)
+        const result =queryString.parse(this.props.location.search)
+        console.log(result)
+    }
+
     render() {
         return (
             <div>
                 <h1>Blogs:</h1>
                 <p>The author is {this.props.match.params.author}</p>
+                <button onClick={this.parseQuery}>show Query</button>
             </div>
         );
     }
 }
-
+// npm install query-string
 export default Blogs;
